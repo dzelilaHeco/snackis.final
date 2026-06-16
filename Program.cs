50509bf51d48139169a473fdd79e41c5bb940b47
dzelilaHeco <dzelila.heco@gmail.com>
2026-06-16 16:53:05 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,48 +1,99 @@
-
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Snackis.Application.Interfaces;
 using Snackis.Application.Services;
+using Snackis.Domain.Entities;
 using Snackis.Domain.Interfaces;
 using Snackis.Infrastructure.Data;
 using Snackis.Infrastructure.Repositories;
+using Snackis.Presentation.Components;
+using Snackis.Presentation.Components.Account;
 
-namespace Snackis.API
+namespace Snackis.Presentation
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddDbContext<MyDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionString") ?? throw new InvalidOperationException("Anslutning till db hittades inte"))
             );
 
+            builder.Services.AddScoped<IPostRepository, PostRepository>();
+            builder.Services.AddScoped<IPrivateMessageRepository, PrivateMessageRepository>();
+            builder.Services.AddScoped<IReportRepository, ReportRepository>();
+            builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+            builder.Services.AddScoped<ITopicRepository,  TopicRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+            builder.Services.AddScoped<IPostService, PostServiceApi>();
+            builder.Services.AddScoped<ICommentService, CommentService>();
+            builder.Services.AddScoped<IPrivateMessageService, PrivateMessageService>();
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<ITopicService, TopicService>();
+            builder.Services.AddScoped<IReportService, ReportService>();
+
+            builder.Services.AddScoped<AdminService>();
+
+            // Efter builder.Services.AddScoped<AdminService>(); lägg till:
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddHttpClient("PostClient", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:7169/");
+            });
+
+            builder.Services.AddCascadingAuthenticationState();
+            builder.Services.AddScoped<IdentityUserAccessor>();
+            builder.Services.AddScoped<IdentityRedirectManager>();
+            builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+                .AddIdentityCookies();
+
+            builder.Services.AddIdentityCore<MyUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<MyDbContext>()
+                .AddSignInManager()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddSingleton<IEmailSender<MyUser>, IdentityNoOpEmailSender>();
+
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (!app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for Postion scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+            app.UseAntiforgery();
 
+            app.UseStaticFiles();
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
 
+            // Efter app.MapRazorComponents... lägg till:
             app.MapControllers();
+
+            app.MapAdditionalIdentityEndpoints();
 
             app.Run();
         }
